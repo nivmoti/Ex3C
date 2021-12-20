fa1ed17498cf85ea3f6cc8f5c9ec12e5bced715d
nivmoti <93876502+nivmoti@users.noreply.github.com>
2021-12-20 23:30:03 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include "my_StringProg.h"
+#include <string.h>
 #define TXT 1024
 #define WORD 30
 
 
 int main(){
-    char text[TXT];
-    char word[WORD];
-    words(word);
+    char Word[WORD];
+     //printf("word:\n");
+    char a;
+    int i=0;
+    scanf("%c",&a);
+    Word[i]=a;
+    i++;
+    while(a!=' '){
+     scanf("%c",&a);
+        Word[i]=a;
+        i++;
+    }
+    char w[WORD];
+    int k=0;
+    for (int j = 0; j < i;j++)
+    {
+        if(Word[j]!='\n'&&Word[j]!='\t'&&Word[j]!=' '){
+            w[k]=Word[j];
+            k++;
+        }
+    }
+    
+    //printf("%s\n",w);
+        char text[TXT];
     texts(text);
-    G(text,word);
-    Atb(text,word);
-    isExist(word,text);
-    return 0;
+    G(text,w);
     
+    Atb(text,w);
+    isExist(w,text);
 
-    
+
+    return 0;
 }
