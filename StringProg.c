b5043ba49aa52059fb7ce04a81d0e30fe0f52a3a
nivmoti <93876502+nivmoti@users.noreply.github.com>
2021-12-20 04:12:08 +0200

    Update StringProg.c

--- StringProg.c
+++ StringProg.c
@@ -5,111 +5,105 @@
 #include "my_StringProg.h"
 
 
-
+//gets word
 void words(char Word[]){
-    char a;
-    int i=0;
-    while (a!=' '&&a!='\t'&&a!='\n'&&i<30)
-    {
-      scanf("%c",&a);
-        Word[i]=a;
-        i++;
-    }
-      
+    printf("enter a word:");
+    // char a;
+    scanf("%s",Word);
+    // int i=0;
+    // while (a!=' '&&a!='\t'&&a!='\n')
+    // {   
+    //     Word[i]=a;
+    //     scanf("%s",Word);
+    //     i++;
+    // }    
 }
+//gets text
 void texts(char text[]){
+    printf("enter a text");
     char a;
     int i=0;
     while (a!='~'&&i<1024)
     {
-      scanf("%c",&a);
-        text[i]=a;
-        i++;
-    }
+        scanf("%c",&a);
+        if (a=='\n')
+        {
+            continue;
+        }
       
+        text[i]=a;
+        i++;   
+    }      
 }
-
-int G(char te[],char wo[])
+//A:
+int G(char* te,char wo[])
 {
     int w=0;
     for (int i = 0; i < strlen(wo); i++)
     {
         if ((wo[i]>64)&&(wo[i]<91))
         {
-            w=w+wo[i]-64;
+            w=w+(int)wo[i]-64;
         }
         if ((wo[i]>96)&&(wo[i]<123))
         {
-            w=w+wo[i]-96;
+            w=w+(int)wo[i]-96;
         }
         
     }
     char ans[TXT]="";
     int temp=0;
-    char bdik[TXT];
-    int k=0;
-    for (int i = 0; (i < strlen(te))||(te[i]!='~'); i++)
-    {   
-        int l=i;
-       while (temp<w&&l<strlen(te))
-       {
-            if ((te[l]>64)&&(te[i]<91))
+    int i=0;
+    while (i<strlen(te))
+    {
+        for (int k = i; k < strlen(te); k++)
         {
-            temp=temp+te[l]-64;
-            
+            if(!(te[k]>64)&&(te[k]<91) && !(te[k]>96)&&(te[k]<123))
+            {
+                i++;
+            }else{
+                break;
+            }
         }
-        if ((te[l]>96)&&(te[l]<123))
+        
+        int j=i;
+        temp=0;
+        while (temp<w&& j < strlen(te))
         {
-            temp=temp+te[l]-96;
+            if ((te[j]>64)&&(te[j]<91))
+            {
+                temp=temp+(int)te[j]-64;
+            }
+            if ((te[j]>96)&&(te[j]<123))
+            {
+                temp=temp+(int)te[j]-96;
+            } 
+            j++;
         }
-         bdik[k]=te[l];
-            k++;
-            l++;
-
-       } 
-       if(temp>=w){
-        if (temp==w)
+        if(w==temp)
         {
-           strcat(ans,bdik);
-           ans[strlen(ans)]='~';
-           k=0;
+            strncat(ans,te+i,j-i);
+            strcat(ans,"~");
+            temp=0;
+               
         }
         if (temp>w)
         {
-            k=0;
+            temp=0;   
         }
-        }else{
-            break;
-       }
-       }
-       ans[strlen(ans)-1]='\n';
-        printf("Gematria Sequences: %s",ans);
-    
+        i++;
+    }
+    ans[strlen(ans)-1]='\n';
+    printf("Gematria Sequences: %s",ans);  
     return 0;
-
 }
 
-int Done(int is[],int size){
-    
-            for (int i = 0; i<size; i++)
-            {
-                if(is[i]==0){
-                    return 0;
-                }
-            }
-            return 1;
-            
-        }
-        
-    
+//B    
 int Atb(char te[],char wo[])
 {
     char op[strlen(wo)];
     char ans[TXT]="";
     char opop[strlen(wo)];
-    
-    //int k=0;
-    //int j=0;
     for (int i = 0; i < strlen(wo); i++)
     {
         if ((wo[i]<=64+13)&&(wo[i]>64))
@@ -133,97 +127,98 @@ int Atb(char te[],char wo[])
     {
         opop[i]=op[strlen(op)-1-i];
     }
-    for (int i = 0; i < strlen(te); i++)
-    {
-        
-        if(te[i]==op[0]){
+    for (int i = 0; i < strlen(te)-1; i++)
+    {    
+        if(te[i]==op[0])
+        {
             int l=i;
             int isCorr=1;
             if(l+1==strlen(te)&&strlen(wo)!=1)
                 isCorr=0;
-            while(isCorr==1&&l+1<strlen(te)){
-                l++;
-                int j = 0;
-                while(j<strlen(op)){ 
-                    if(te[l]==' '||te[l]=='\t'||te[l]=='\n'){
-                        continue;
-                    }
-                    if(te[l]!=op[j]){
-                        isCorr=0;
-                        break;
-                    }else{
+            l++;
+            int j = 1;
+            int k=1;
+            while(j<strlen(op)&&isCorr==1&&l<strlen(te))
+            {  
+                if(te[l]==' '||te[l]=='\t'||te[l]=='\n')
+                {
+                    k++;
+                    continue;
+                }
+                if(te[l]!=op[j]){
+                    isCorr=0;
+                    break;
+                }else{
+                        k++;
                         j++;
+                        l++;
                     }
-                }
-                if(j==strlen(op)){
-                    char temp[l];
-                    int h=0;
-                    for (int m = i; m < l; m++)
-                    {
-                        temp[h]=te[m];
-                        h++;
-                    }
-                    strcat(temp,"~");
-                    strcat(ans,temp);
-                }
-                
             }
-            
+            if(j==strlen(op)&&isCorr==1)
+            {
+                strncat(ans,te+i,k);
+                strcat(ans,"~");
+            }  
         }
-        if(te[i]==opop[0]){
+        if(te[i]==opop[0])
+        {
             int l=i;
             int isCorr=1;
-             if(l+1==strlen(te)&&strlen(wo)!=1)
+            if(l+1==strlen(te)&&strlen(wo)!=1)
                 isCorr=0;
-            while(isCorr==1){
-                l++;
-                int j = 0;
-                while(j<strlen(opop)){ 
-                    if(te[l]==' '||te[l]=='\t'||te[l]=='\n'){
-                        continue;
-                    }
-                    if(te[l]!=opop[j]){
-                        isCorr=0;
-                        break;
-                    }else{
-                        j++;
-                    }
+            l++;
+            int j = 1;
+            int k=1;
+            while(j<strlen(opop)-1&&isCorr==1&&l<strlen(te))
+            {
+                if(te[l]==' '||te[l]=='\t'||te[l]=='\n')
+                {
+                    k++;
+                    continue;
                 }
-                if(j==strlen(opop)){
-                   char temp[l];
-                    int h=0;
-                    for (int m = i; m < l; m++)
-                    {
-                        temp[h]=te[m];
-                        h++;
-                    }
-                    strcat(temp,"~");
-                    strcat(ans,temp);
-                }   
-            }   
+                if(te[l]!=opop[j])
+                {
+                    isCorr=0;
+                    break;
+                }else{
+                    k++;
+                    j++;
+                    l++;
+                }
+            }
+            if(j==strlen(opop)-1&&isCorr==1)
+            {
+                strncat(ans,te+i,k);
+                strcat(ans,"~");
+            }      
         }
-
-    
-       
     }
     ans[strlen(ans)-1]='\n'; 
     printf("Atbash Sequences: %s",ans);
-    return 0;
-        
+    return 0;   
+}
+ //helper function for C:   
+int Done(int is[],int size){
+            for (int i = 0; i<size; i++)
+            {
+                if(is[i]==0){
+                    return 0;
+                }
+            }
+            return 1;
     }
-    
-
+//c:
 int isExist(char word[],char text[]){
     int isexist[strlen(word)];
     char an[TXT]="";
-    char ans[strlen(text)];
+    char ans[TXT]="";
     int k=0;
-    for (int i = 0; i < strlen(word); i++)
-    {
-        isexist[i]=0;
-    }
     for (int i = 0; i < strlen(text); i++)
     {
+        for (int i = 0; i < strlen(word); i++)
+        {
+            isexist[i]=0;
+        }
         int isStart=0;
         for (int j = 0; j < strlen(word); j++)
         {
@@ -234,51 +229,55 @@ int isExist(char word[],char text[]){
                 k++;
                 break;
             }
-        }
-        
-        if(isStart==1){
-        int l=i;
-        int booleanIs=1;
-        int end=Done(isexist,sizeof(isexist));
-        while (end==0&&booleanIs==1&&l<strlen(text))
+        } 
+        //text[i] is in the word
+        if(isStart==1)
         {
-            booleanIs=0;
-             if(text[l]==' '){
-            ans[k]=text[l];
-            k++; 
-            booleanIs=1;
- 
-        }
-        for (int j = 0; j < strlen(word); j++)
-        {
-            if(text[l]==word[j]&&isexist[j]==0){
-                booleanIs=1;
-                isexist[j]=1;
+            int l=i+1;
+            int booleanIs=1;
+            int end=Done(isexist,sizeof(isexist)/4);
+            while (end==0&&booleanIs==1&&l<strlen(text))
+            {
+                booleanIs=0;
+                if(text[l]==' ')
+                {
                 ans[k]=text[l];
-                k++;
-                break;
+                k++; 
+                booleanIs=1;
+                }
+                for (int j = 0; j < strlen(word); j++)
+                {
+                    if(text[l]==word[j]&&isexist[j]==0){
+                        booleanIs=1;
+                        isexist[j]=1;
+                        ans[k]=text[l];
+                        k++;
+                        break;
+                    }
+                }
+                end=Done(isexist,sizeof(isexist)/4);
+                l++;
+        
             }
-        }
-        end=Done(isexist,sizeof(isexist));
-        l++;
-        }
-        if(end==1){
-            strcat(an,ans);
-            strcat(an,"~");   
-        }else{ if(l==strlen(text)){
-            break;
-        }else{
-            k=0;
-             for (int i = 0; i < strlen(word); i++)
-    {
-        isexist[i]=0;
-    }
-        }
-        }
+            //we find all the leters 
+            if(end==1)
+            {
+                strcat(an,ans);
+                strcat(an,"~"); 
+            //we didnt find the leters  
+            }else{ if(l==strlen(text)) 
+                    {
+                        break;
+                    }else{
+                        i=l-1;
+                        strcpy(ans,"");
+                         k=0;
+           
+                        }
+                 }
         }
     }
     an[strlen(an)-1]='\n';
     printf("Anagram Sequences: %s",an);
     return 0;
-
 }
